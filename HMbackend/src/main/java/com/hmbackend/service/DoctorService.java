77320674118b9e44943ecc96891cffaf9c365239
赵嘉兴 <2634439734@qq.com>
2022-03-20 13:39:48 +0800

    修改了doctor增加患者的方法

--- HMbackend/src/main/java/com/hmbackend/service/DoctorService.java
+++ HMbackend/src/main/java/com/hmbackend/service/DoctorService.java
@@ -1,6 +1,7 @@
 package com.hmbackend.service;
 
 import com.hmbackend.mapper.DoctorMapper;
+import com.hmbackend.mapper.PatientMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +9,14 @@ import org.springframework.stereotype.Service;
 public class DoctorService {
     @Autowired
     private DoctorMapper doctorMapper;
+    @Autowired
+    private PatientMapper patientMapper;
+    public String addPatient(String patientId){
+        if(patientMapper.queryPatientById(patientId)==null){
+            return"此患者未注册";
+        }else{
+         return"";
+        }
+
+    }
 }
