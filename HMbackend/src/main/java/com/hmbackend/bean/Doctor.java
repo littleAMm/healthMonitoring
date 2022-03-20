f46e7c4c96985716b0b615351333fbdc7db9aeed
赵嘉兴 <2634439734@qq.com>
2022-03-20 13:39:48 +0800

    change the class

---
+++ HMbackend/src/main/java/com/hmbackend/bean/Doctor.java
@@ -0,0 +1,15 @@
+package com.hmbackend.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Doctor extends User{
+    private int id;
+    private String name;
+    private String sex;
+    private String work;
+}
