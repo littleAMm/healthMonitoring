499de8bd79472937966daef33fdcd1e88bc8c358
赵嘉兴 <2634439734@qq.com>
2022-03-20 13:07:39 +0800

    Initial commit

---
+++ HMbackend/src/main/java/com/example/hmbackend/HMbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HMbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HMbackendApplication.class, args);
+    }
+
+}
