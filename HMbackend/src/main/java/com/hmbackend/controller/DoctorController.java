0161fa13d0360efd51c4fb41a1ee3ecd90ac3f7e
赵嘉兴 <2634439734@qq.com>
2022-03-20 13:39:48 +0800

    修改了doctor

--- HMbackend/src/main/java/com/hmbackend/controller/DoctorController.java
+++ HMbackend/src/main/java/com/hmbackend/controller/DoctorController.java
@@ -2,10 +2,13 @@ package com.hmbackend.controller;
 
 import com.hmbackend.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/doctor")
 public class DoctorController {
     @Autowired
     private DoctorService doctorService;
+
 }
