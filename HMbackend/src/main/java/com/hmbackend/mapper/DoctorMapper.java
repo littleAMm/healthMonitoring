914bec43212a170af816d90ab4d6ccc90d011a6e
赵嘉兴 <2634439734@qq.com>
2022-03-20 13:39:48 +0800

    change sql

--- HMbackend/src/main/java/com/hmbackend/mapper/DoctorMapper.java
+++ HMbackend/src/main/java/com/hmbackend/mapper/DoctorMapper.java
@@ -4,7 +4,13 @@ package com.hmbackend.mapper;
 import com.hmbackend.bean.Patient;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface DoctorMapper {
+    //添加自己的患者
     int addPatient(Patient patient);
+    int deletePatient(Patient patient);
+    //查询自己的患者
+    List<Patient> queryAllPatient();
 }
--- HMbackend/src/main/java/com/hmbackend/service/DoctorService.java
+++ HMbackend/src/main/java/com/hmbackend/service/DoctorService.java
@@ -1,9 +1,16 @@
 package com.hmbackend.service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.hmbackend.bean.Doctor;
+import com.hmbackend.bean.Patient;
 import com.hmbackend.mapper.DoctorMapper;
 import com.hmbackend.mapper.PatientMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DoctorService {
