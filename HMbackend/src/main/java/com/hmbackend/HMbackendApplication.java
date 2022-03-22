fee2d82c293c916d0c6d4ac664dde4b456fc70ab
yexuan <xtzs1015@qq.com>
2022-03-22 19:51:30 +0800

    add mapper annotation

--- HMbackend/src/main/java/com/hmbackend/HMbackendApplication.java
+++ HMbackend/src/main/java/com/hmbackend/HMbackendApplication.java
@@ -1,9 +1,11 @@
 package com.hmbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.hmbackend.mapper"})
 public class HMbackendApplication {
 
     public static void main(String[] args) {
