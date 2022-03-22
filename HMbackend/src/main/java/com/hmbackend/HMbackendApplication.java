cd3f591345fd25920d8cc756aacc1b7b1e109111
yexuan <xtzs1015@qq.com>
2022-03-22 19:51:30 +0800

    add mapper annotation

--- HMbackend/src/main/java/com/hmbackend/HMbackendApplication.java
+++ HMbackend/src/main/java/com/hmbackend/HMbackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.hmbackend.mapper"})
 public class HMbackendApplication {
 
     public static void main(String[] args) {
