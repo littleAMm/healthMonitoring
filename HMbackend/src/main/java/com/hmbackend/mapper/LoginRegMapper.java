541a8b8ed15b1a53d0c1fbe1f73c97a350b71d14
yexuan <xtzs1015@qq.com>
2022-03-26 18:05:01 +0800

    init test

---
+++ HMbackend/src/main/java/com/hmbackend/mapper/LoginRegMapper.java
@@ -0,0 +1,14 @@
+package com.hmbackend.mapper;
+
+
+import com.hmbackend.bean.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface LoginRegMapper {
+    //注册用
+    int register(User user);
+    //登录用
+    User login(@Param("username") String username,@Param("password") String password);
+}
--- HMbackend/src/test/java/com/hmbackend/HMbackendApplicationTests.java
+++ HMbackend/src/test/java/com/hmbackend/HMbackendApplicationTests.java
@@ -1,13 +1,28 @@
 package com.hmbackend;
 
+import com.hmbackend.bean.User;
+import com.hmbackend.mapper.LoginRegMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HMbackendApplicationTests {
 
+
+    @Autowired
+    LoginRegMapper loginRegMapper;
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test() {
+        User test = loginRegMapper.login("test", "1");
+        if(test.getRole().equals("医生"))
+        {
+
+        }
     }
 
 }
