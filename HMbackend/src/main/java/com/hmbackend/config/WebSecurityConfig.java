c77207abbbdaf9b789fbfa841c6c4bf0178baf5c
yexuan <xtzs1015@qq.com>
2022-03-26 11:24:05 +0800

    lately use

---
+++ HMbackend/src/main/java/com/hmbackend/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+//package com.hmbackend.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+///**
+// * @author_name:xiatao
+// * @data:2022/3/24
+// * @time:17:38
+// */
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class WebSecurityConfig {
+//
+//}
