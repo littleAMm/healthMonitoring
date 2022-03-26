c32a8e91ab124a15dae45942c26214ff5f20e0a6
yexuan <xtzs1015@qq.com>
2022-03-26 18:05:01 +0800

    init test

--- HMbackend/src/main/java/com/hmbackend/controller/HelloController.java
+++ HMbackend/src/main/java/com/hmbackend/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.hmbackend.controller;
 
+import com.hmbackend.bean.User;
+import com.hmbackend.mapper.LoginRegMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,13 +17,32 @@ import org.springframework.web.bind.annotation.RestController;
  * 测试用
  */
 
-@RequestMapping("/hello")
 @RestController
 @Api(tags = "HelloController")
 public class HelloController {
-    @RequestMapping("/t1")
+    @Autowired
+    LoginRegMapper loginRegMapper;
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
     @ApiOperation("hello")
-    public String hello(){
-        return "hello";
+    public String hello(@RequestParam("username") String username,
+                        @RequestParam("password") String password){
+        User user = loginRegMapper.login(username, password);
+        if (user.getRole().equals("医生")){
+            return "redirect:/doctor.html";
+        }else {
+            return "失败";
+        }
+    }
+    @RequestMapping(value = "/reg",method = RequestMethod.POST)
+    public String reg(@RequestParam("username") String username,
+                      @RequestParam("password") String password){
+        User user = new User(username,password);
+        if(loginRegMapper.register(user)==1){
+            return "注册成功";
+        }
+        else{
+            return "false";
+        }
     }
 }
