88754b47227135c0baa337d39c22cc3562bd40b5
yexuan <xtzs1015@qq.com>
2022-03-26 20:00:15 +0800

    simple login and register test

---
+++ HMbackend/src/main/java/com/hmbackend/controller/LoginController.java
@@ -0,0 +1,45 @@
+package com.hmbackend.controller;
+
+import com.hmbackend.service.LoginService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author_name:xiatao
+ * @data:2022/3/24
+ * @time:17:17
+ * 测试用
+ */
+
+@RestController
+@Api(tags = "Login")
+public class LoginController {
+    @Autowired
+    LoginService loginService;
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        HttpServletResponse response) throws IOException {
+        String result =  loginService.login(username,password);
+        if (result.charAt(0)=='/'){
+            response.sendRedirect(result);
+            return "";
+        }else {
+            return result;
+        }
+    }
+
+    @RequestMapping(value = "/reg",method = RequestMethod.POST)
+    public String register(@RequestParam("username") String username,
+                           @RequestParam("password") String password){
+        return loginService.register(username,password);
+    }
+}
--- HMbackend/src/main/java/com/hmbackend/mapper/LoginRegMapper.java
+++ HMbackend/src/main/java/com/hmbackend/mapper/LoginRegMapper.java
@@ -9,6 +9,8 @@ import org.apache.ibatis.annotations.Param;
 public interface LoginRegMapper {
     //注册用
     int register(User user);
+    int queryUserByName(@Param("username") String username);
+
     //登录用
     User login(@Param("username") String username,@Param("password") String password);
 }
