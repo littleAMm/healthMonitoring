43d73117e8a4ee4597b5f9b473b10372742881ce
yexuan <xtzs1015@qq.com>
2022-04-14 14:18:02 +0800

    add patient

--- HMbackend/src/main/java/com/hmbackend/mapper/PatientMapper.java
+++ HMbackend/src/main/java/com/hmbackend/mapper/PatientMapper.java
@@ -1,6 +1,10 @@
 package com.hmbackend.mapper;
 
+import com.hmbackend.bean.Doctor;
+import com.hmbackend.bean.Health;
+import com.hmbackend.bean.Patient;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * @author_name:xiatao
@@ -10,5 +14,15 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface PatientMapper {
+    Patient queryPatientById(@Param("patientId") int patientId);
 
+    int addDoctor(@Param("doctorId") int doctorId,@Param("patientId") int patientId);
+
+    Doctor queryDoctorById(@Param("doctorId")int doctorId);
+
+    int deleteDoctor(@Param("id") int id);
+
+    int creatHealth(Health health);
+
+    int updateHeath(@Param("patientId") int id, @Param("status") String status,@Param("temp") double temp);
 }
